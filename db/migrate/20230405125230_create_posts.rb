a9ba550c3a080ab8078d34858f46d5abe91f3fc3
Utsargo Roy <royutsargo@gmail.com>
2023-04-05 18:53:52 +0600

    Add Posts migration file

---
+++ db/migrate/20230405125230_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.integer :AuthorId
+      t.string :Title
+      t.string :Text
+      t.datetime :CreatedAt
+      t.datetime :UpdatedAt
+      t.integer :CommentsCounter
+      t.integer :LikesCounter
+      t.timestamps
+    end
+  end
+end
